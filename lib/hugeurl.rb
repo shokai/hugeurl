b7e17cbde166c1f4fe843839077d97c999a44e7b
Sho Hashimoto <hashimoto@shokai.org>
2012-04-11 01:53:33 +0900

    support HTTPS

--- lib/hugeurl.rb
+++ lib/hugeurl.rb
@@ -4,13 +4,19 @@ $:.unshift(File.dirname(__FILE__)) unless
 require 'rubygems'
 require 'uri'
 require 'net/http'
+require 'net/https'
 
 module Hugeurl
-  VERSION = '0.0.4'
+  VERSION = '0.0.5'
   def self.get(url)
     url = URI.parse url unless url.class.to_s =~ /^URI::/
-    res = Net::HTTP.start(url.host, url.port).
-      head(url.path.size < 1 ? '/' : url.path)
+    if url.class == URI::HTTPS
+      http = Net::HTTP.new(url.host, 443)
+      http.use_ssl = true
+    else
+      http = Net::HTTP.new(url.host, url.port)
+    end
+    res = http.head(url.path.size < 1 ? '/' : url.path)
     URI.parse res['location'] rescue return url
   end
 end
